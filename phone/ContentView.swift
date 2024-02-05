c0df51ec0f34a134909ad4d6266a0593125a6f57
Kgaugelo Ramaramela <kgaugelo.ramaramela@standardbank.co.za>
2024-02-05 16:15:59 +0200

    Initial Commit

---
+++ phone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  phone
+//
+//  Created by Ramaramela, Kgaugelo K on 2024/02/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ phone/phoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  phoneApp.swift
+//  phone
+//
+//  Created by Ramaramela, Kgaugelo K on 2024/02/05.
+//
+
+import SwiftUI
+
+@main
+struct phoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
